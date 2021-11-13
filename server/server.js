04c7139bb926ddeeb57e44c4c58b4e169791c24f
Felix Luciano <felixluciano.a@gmail.com>
2021-11-13 14:19:44 -0500

    Added mongodb connection

--- server/server.js
+++ server/server.js
@@ -1,7 +1,24 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+// Mongoose connection
+mongoose.connect(
+  process.env.MONGO
+).catch(()=>console.log("Something went wrong dude"));
+
+// db 
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error: "));
+
+db.once("open", function () {
+  console.log("Connected successfully");
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
